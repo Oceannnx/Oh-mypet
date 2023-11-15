7ffe34d693fa63197b6e239974b8e7b36de47678
หนมจีบ <onopingping@gmail.com>
2023-11-15 17:54:09 +0700

    add Post element

--- Client/src/page/Sellpost/index.jsx
+++ Client/src/page/Sellpost/index.jsx
@@ -1,13 +1,5 @@
 import React from 'react'
 
 export const Sellpost = () => {
-  return (
-    <>
-      <div>
-        Loremm ipsum, dolor sit amet consectetur adipisicing elit. Eveniet optio soluta atque excepturi architecto sequi
-        rem iusto corrupti. Enim repellendus dolor magni exercitationem perspiciatis nam eligendi odit, adipisci beatae
-        placeat.
-      </div>
-    </>
-  )
+  return <></>
 }
