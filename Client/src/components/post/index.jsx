2926ea88abb39066857882c5f737c9a706eb12fa
naphatchanun <111549617+naphatchanun@users.noreply.github.com>
2023-11-15 23:43:19 +0700

    login

--- Client/src/components/post/index.jsx
+++ Client/src/components/post/index.jsx
@@ -6,11 +6,7 @@ export const Post = () => {
       <div>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, consectetur nisi doloremque facilis deserunt
         commodi voluptate. Libero facilis expedita quas, natus consequuntur dolor, illum suscipit quae recusandae cumque
-        distinctio minima! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, quasi tempora! Minima harum
-        sit natus, hic earum dignissimos cumque laboriosam autem dolores, odit impedit at animi, repellat provident
-        facilis recusandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, cumque architecto quas alias
-        rerum delectus laudantium animi reiciendis neque eligendi enim numquam, ab minima laboriosam ipsa, possimus
-        tenetur dolorem doloremque!
+        distinctio minima!
       </div>
     </>
   )
