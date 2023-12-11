60b87d3efacd50d3061095c0cc74b3c932ad53da
SSSBoOm <boom1909chanapat@gmail.com>
2023-12-12 05:52:57 +0700

    feat: style

--- Client/src/page/Homepage/index.jsx
+++ Client/src/page/Homepage/index.jsx
@@ -27,7 +27,7 @@ export const Homepage = () => {
       ) : (
         <div className="bg-[#FFFDF3] py-[30px]">
           <div className="flex justify-center">
-            <div className="md:grid md:grid-cols-2 md:m-0 m-6 grid-cols-1 border rounded-lg border-gray-400 w-[1320px] bg-[#E0F1F5] drop-shadow-lg">
+            <div className="md:grid md:grid-cols-2 md:m-3 m-6 grid-cols-1 border rounded-lg border-gray-400 w-[1320px] bg-[#E0F1F5] drop-shadow-lg">
               <div className="flex justify-center items-center md:py-[200px] py-[25px] md:text-2xl text-xl text-blue-900">
                 <h1>WELCOME TO OH-MYPET</h1>
               </div>
@@ -36,27 +36,25 @@ export const Homepage = () => {
               </div>
             </div>
           </div>
-          <div className="flex bg-secondaryColor justify-center py-8">
-            <div className="mx-5 flex flex-wrap">
-              {posts.map((post, index) => {
-                return (
-                  <Post
-                    key={index}
-                    userId={post.user._id}
-                    email={post.user.email}
-                    fName={post.user.fName}
-                    lName={post.user.lName}
-                    title={post.title}
-                    petType={post.petType}
-                    price={post.petPrice}
-                    location={post.petLocation}
-                    petImage={post.petImages}
-                    postDate={post.petPostDate}
-                    postId={post._id}
-                  />
-                )
-              })}
-            </div>
+          <div className="flex flex-wrap gap-x-4 gap-y-4 justify-center bg-secondaryColor py-8 md:m-3 m-6">
+            {posts.map((post, index) => {
+              return (
+                <Post
+                  key={index}
+                  userId={post.user._id}
+                  email={post.user.email}
+                  fName={post.user.fName}
+                  lName={post.user.lName}
+                  title={post.title}
+                  petType={post.petType}
+                  price={post.petPrice}
+                  location={post.petLocation}
+                  petImage={post.petImages}
+                  postDate={post.petPostDate}
+                  postId={post._id}
+                />
+              )
+            })}
           </div>
         </div>
       )}
