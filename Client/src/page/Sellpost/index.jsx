02968a1ae8cb48cce2434d3e6dbc69b02625831d
naphatchanun <111549617+naphatchanun@users.noreply.github.com>
2023-12-11 22:03:00 +0700

    SellPost
    -เเก้พื้นหลัง

--- Client/src/page/Sellpost/index.jsx
+++ Client/src/page/Sellpost/index.jsx
@@ -24,64 +24,74 @@ export const Sellpost = () => {
   const BD = new Date(sellpost[0]?.petBD)
   return (
     <>
-      {isLoading ? (
-        <div className="flex justify-center items-center h-screen bg-[#FFFDF3]">
-          <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue1"></div>
-        </div>
-      ) : (
-        <div>
+      <div className="h-screen bg-[#FFFDF3]">
+        {isLoading ? (
+          <div className="flex justify-center items-center h-screen bg-[#FFFDF3]">
+            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue1"></div>
+          </div>
+        ) : (
           <div>
-            {sellpost[0].user.fName} {sellpost[0].user.lName}
+            <div className="grid grid-cols-2">
+              <div className="flex justify-center">
+                <div className="flex justify-center py-5">
+                  {sellpost[0].user.fName} {sellpost[0].user.lName}
+                </div>
+                <div className="flex justify-center items-center py-5">
+                  <img src={sellpost[0].petImages}></img>
+                </div>
+              </div>
+              <div>
+                <div>{sellpost[0].title}</div>
+                <div>{sellpost[0].petAge}</div>
+                <div>{BD.toLocaleDateString('th-TH', { year: 'numeric', month: 'long', day: 'numeric' })}</div>
+                <div>{sellpost[0].petDescription}</div>
+                <div>{sellpost[0].petGender}</div>
+                <div>{sellpost[0].petGene}</div>
+                <div>{sellpost[0].petLocation}</div>
+                <div>{sellpost[0].petName}</div>
+                <div>{postDate.toLocaleDateString('th-TH', { year: 'numeric', month: 'long', day: 'numeric' })}</div>
+                <div>{sellpost[0].petPrice}</div>
+                <div>{sellpost[0].petType}</div>
+                <a href="https://www.blacklistseller.com/report/report_search_page" className="text-red-600 text-3xl">
+                  {' '}
+                  Check Blacklist !!!
+                </a>
+                <div className="flex">
+                  {sellpost[0].user.facebook === null ? (
+                    <></>
+                  ) : (
+                    <a className="w-12 mx-1" href={sellpost[0].user.facebook}>
+                      <img src="\src\assets\facebook.png"></img>
+                    </a>
+                  )}
+                  {sellpost[0].user.line === null ? (
+                    <></>
+                  ) : (
+                    <a className="w-12 mx-1" href={sellpost[0].user.line}>
+                      <img src="\src\assets\line.png"></img>
+                    </a>
+                  )}
+                  {sellpost[0].user.twitter === null ? (
+                    <></>
+                  ) : (
+                    <a className="w-12 mx-1" href={sellpost[0].user.twitter}>
+                      <img src="\src\assets\twitter.png"></img>
+                    </a>
+                  )}
+                  {sellpost[0].user.instagram === null ? (
+                    <></>
+                  ) : (
+                    <a className="w-12 mx-1" href={sellpost[0].user.instagram}>
+                      <img src="\src\assets\instagram.png"></img>
+                    </a>
+                  )}
+                </div>
+              </div>
+            </div>
           </div>
-          <div>{sellpost[0].title}</div>
-          <div>{sellpost[0].petAge}</div>
-          <div>{BD.toLocaleDateString('th-TH', { year: 'numeric', month: 'long', day: 'numeric' })}</div>
-          <div>{sellpost[0].petDescription}</div>
-          <div>{sellpost[0].petGender}</div>
-          <div>{sellpost[0].petGene}</div>
-          <img src={sellpost[0].petImages}></img>
-          <div>{sellpost[0].petLocation}</div>
-          <div>{sellpost[0].petName}</div>
-          <div>{postDate.toLocaleDateString('th-TH', { year: 'numeric', month: 'long', day: 'numeric' })}</div>
-          <div>{sellpost[0].petPrice}</div>
-          <div>{sellpost[0].petType}</div>
-          <a href="https://www.blacklistseller.com/report/report_search_page" className="text-red-600 text-3xl">
-            {' '}
-            Check Blacklist !!!
-          </a>
-          <div className="flex">
-            {sellpost[0].user.facebook === null ? (
-              <></>
-            ) : (
-              <a className="w-12 mx-1" href={sellpost[0].user.facebook}>
-                <img src="\src\assets\facebook.png"></img>
-              </a>
-            )}
-            {sellpost[0].user.line === null ? (
-              <></>
-            ) : (
-              <a className="w-12 mx-1" href={sellpost[0].user.line}>
-                <img src="\src\assets\line.png"></img>
-              </a>
-            )}
-            {sellpost[0].user.twitter === null ? (
-              <></>
-            ) : (
-              <a className="w-12 mx-1" href={sellpost[0].user.twitter}>
-                <img src="\src\assets\twitter.png"></img>
-              </a>
-            )}
-            {sellpost[0].user.instagram === null ? (
-              <></>
-            ) : (
-              <a className="w-12 mx-1" href={sellpost[0].user.instagram}>
-                <img src="\src\assets\instagram.png"></img>
-              </a>
-            )}
-          </div>
-          <Footer></Footer>
-        </div>
-      )}
+        )}
+      </div>
+      <Footer></Footer>
     </>
   )
 }
