f60ce87aa3584a8ed5f1095779a43beaefb0f55c
หนมจีบ <onopingping@gmail.com>
2023-12-14 03:56:54 +0700

    feat : new css all post page

--- Client/src/page/Homepage/index.jsx
+++ Client/src/page/Homepage/index.jsx
@@ -40,7 +40,7 @@ export const Homepage = () => {
               </div>
             </div>
           </div>
-          <div className="flex flex-wrap gap-x-4 gap-y-4 justify-center bg-secondaryColor py-8 md:m-3 m-6">
+          <div className="grid grid-cols-4 justify-items-center gap-y-10 bg-secondaryColor py-8 md:m-3 m-6">
             {posts.map((post, index) => {
               return (
                 <Post
