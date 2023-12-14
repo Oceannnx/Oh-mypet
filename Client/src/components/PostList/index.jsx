4daf5442af6503c65eb600784947e2532d1d02df
หนมจีบ <onopingping@gmail.com>
2023-12-15 05:41:43 +0700

    fix : remove h-screen

--- Client/src/components/PostList/index.jsx
+++ Client/src/components/PostList/index.jsx
@@ -20,7 +20,7 @@ export const PostList = (props) => {
     fetchSellPost()
   }, [])
   return (
-    <section className="h-screen bg-[#FFFDF4]">
+    <section className=" bg-[#FFFDF4]">
       {isLoading ? (
         <div className="flex justify-center items-center h-screen bg-[#FFFDF3]">
           <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue1"></div>
