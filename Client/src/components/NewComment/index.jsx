f4cfff1e15a30b1db8a5fe94e804c621936f6160
หนมจีบ <onopingping@gmail.com>
2023-12-14 21:18:53 +0700

    feat : no loggin can view and adding loading

--- Client/src/components/NewComment/index.jsx
+++ Client/src/components/NewComment/index.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react'
 import { AuthContext } from '../../context/user'
 import { AxiosLib } from '../../lib/axios'
 import Swal from 'sweetalert2'
+import { Link } from 'react-router-dom'
 
 export const NewComment = (props) => {
   const { advPostID } = props || ''
@@ -27,27 +28,41 @@ export const NewComment = (props) => {
   }
   return (
     <>
-      <div className="flex m-6 justify-between">
-        <div className="flex w-full justify-start">
-          <img
-            className="rounded-lg w-9 h-9 cursor-pointer"
-            src={`https://avatar.vercel.sh/${auth?.authContext?.fName + auth?.authContext?.lName}.svg?text=${
-              auth?.authContext?.fName[0] + auth?.authContext?.lName[0]
-            }`}
-          ></img>
-          <input
-            onChange={handleOnchange}
-            type="text"
-            className="border overflow-auto mx-5 mb-4 w-full h-9 p-2"
-          ></input>
+      {auth?.authContext.IsLogin ? (
+        <div className="flex m-6 justify-between">
+          <div className="flex w-full justify-start">
+            <img
+              className="rounded-lg w-9 h-9 cursor-pointer"
+              src={`https://avatar.vercel.sh/${auth?.authContext?.fName + auth?.authContext?.lName}.svg?text=${
+                auth?.authContext?.fName[0] + auth?.authContext?.lName[0]
+              }`}
+            ></img>
+            <input
+              onChange={handleOnchange}
+              type="text"
+              className="border overflow-auto mx-5 mb-4 w-full h-9 p-2"
+            ></input>
+          </div>
+          <button
+            onClick={handleComment}
+            className="flex items-center break-all bg-slate-500 text-white rounded-lg w-36 p-5 h-9 mx-5 mb-4"
+          >
+            Comment
+          </button>
         </div>
-        <button
-          onClick={handleComment}
-          className="flex items-center break-all bg-slate-500 text-white rounded-lg w-36 p-5 h-9 mx-5 mb-4"
-        >
-          Comment
-        </button>
-      </div>
+      ) : (
+        <div className="fixed bottom-5 left-1/2 -translate-x-1/2 rounded-lg text-lg flex bg-primaryColor px-5 py-1">
+          <div className="flex justify-center items-center rounded-lg w-full p-5 h-9 text-black">
+            Please login to comment
+          </div>
+          <nobr className="flex justify-center items-center group transition duration-300 text-red-500">
+            <Link to="/login">
+              Login now!
+              <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-red-500"></span>
+            </Link>
+          </nobr>
+        </div>
+      )}
     </>
   )
 }
--- Client/src/page/Advidence/index.jsx
+++ Client/src/page/Advidence/index.jsx
@@ -1,60 +1,52 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Footer } from '../../components/Footer'
 import { AxiosLib } from '../../lib/axios'
 import { AdvPost } from '../../components/AdvPost'
-import { AuthContext } from '../../context/user'
-import Swal from 'sweetalert2'
 
 export const Advidence = () => {
   const [advPosts, setAdvPost] = useState([])
-  const auth = useContext(AuthContext)
+  const [isLoading, setIsLoading] = useState(true)
 
   const fetchAdvPost = async () => {
     try {
       const result = await AxiosLib.get('/api/fetchadvpost')
       setAdvPost(result.data)
+      setIsLoading(false)
     } catch (error) {
       console.log(error)
     }
   }
   useEffect(() => {
     fetchAdvPost()
-    if (!auth?.authContext?.IsLogin) {
-      Swal.fire({
-        title: 'Error!',
-        text: 'Please login first.',
-        icon: 'error',
-        confirmButtonText: 'OK',
-      })
-      setTimeout(() => {
-        window.location.href = '/login'
-      }, 1500)
-    }
-  }, [])
+  })
   return (
     <>
-      {auth?.authContext?.IsLogin ? (
-        <div>
-          <div className="flex bg-secondaryColor flex-col justify-items-center">
-            {advPosts.map((advPost, index) => {
-              return (
-                <AdvPost
-                  key={index}
-                  postDate={advPost.postDate}
-                  postDesc={advPost.postDesc}
-                  title={advPost.title}
-                  userId={advPost.user._id}
-                  fName={advPost.user.fName}
-                  lName={advPost.user.lName}
-                  advPostID={advPost._id}
-                />
-              )
-            })}
-          </div>
-          <Footer />
+      {isLoading ? (
+        <div className="h-screen bg-[#FFFDF3] flex justify-center items-center">
+          <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue1"></div>
         </div>
       ) : (
-        ''
+        <div>
+          <div>
+            <div className="flex bg-secondaryColor flex-col justify-items-center">
+              {advPosts.map((advPost, index) => {
+                return (
+                  <AdvPost
+                    key={index}
+                    postDate={advPost.postDate}
+                    postDesc={advPost.postDesc}
+                    title={advPost.title}
+                    userId={advPost.user._id}
+                    fName={advPost.user.fName}
+                    lName={advPost.user.lName}
+                    advPostID={advPost._id}
+                  />
+                )
+              })}
+            </div>
+            <Footer />
+          </div>
+        </div>
       )}
     </>
   )
