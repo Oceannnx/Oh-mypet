08bf782709ccc96f650cb00046f407fc3f02b626
หนมจีบ <onopingping@gmail.com>
2023-12-15 22:51:02 +0700

    feat : add responsive

--- Client/src/page/Sellpost/index.jsx
+++ Client/src/page/Sellpost/index.jsx
@@ -39,73 +39,78 @@ export const Sellpost = () => {
             <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue1"></div>
           </div>
         ) : (
-          <div className="grid grid-cols-2 m-6">
+          <div className="grid grid-col-1 xl:grid-cols-2 m-6">
             <div className="flex flex-col border-4 rounded-lg p-5">
               <div className="flex pb-4">
                 <img
-                  className="rounded-lg w-20 h-20 cursor-pointer"
+                  className="xl:rounded-lg rounded-md w-11 h-11 xl:w-20 xl:h-20 cursor-pointer"
                   onClick={handleOnclickProfile}
                   src={`https://avatar.vercel.sh/${sellpost[0].user.fName + sellpost[0].user.lName}.svg?text=${
                     sellpost[0].user.fName[0] + sellpost[0].user.lName[0]
                   }`}
                 ></img>
-                <div className="px-6">
-                  <div onClick={handleOnclickProfile} className="text-xl">
+                <div className="xl:px-6 px-3">
+                  <div onClick={handleOnclickProfile} className="xl:text-xl text-lg cursor-pointer">
                     {sellpost[0].user.fName} {sellpost[0].user.lName}
                   </div>
-                  <div className="text-base">
+                  <div className="xl:text-base text-sm">
                     {postDate.toLocaleDateString('th-TH', { year: 'numeric', month: 'long', day: 'numeric' })}
                   </div>
                 </div>
               </div>
-              <div className="flex justify-center">
-                <img src={sellpost[0].petImages}></img>
+              <div className="flex justify-center w-full ">
+                <img className="w-1/2 xl:w-3/4 " src={sellpost[0].petImages}></img>
               </div>
             </div>
             <divv className="flex flex-col p-5">
               <div className="grid grid-cols-2 divide-x-2">
-                <div className="px-10">
-                  <div className="flex py-4 text-2xl">
+                <div className="xl:px-10 px-">
+                  <div className="flex py-4 lg:text-xl text-sm">
                     <div className="pr-4">Title : </div>
                     <div>{sellpost[0].title}</div>
                   </div>
-                  <div className="flex py-4 text-2xl">
+                  <div className="flex py-4 lg:text-xl text-sm">
                     <div className="pr-4">Name : </div>
                     <div>{sellpost[0].petName}</div>
                   </div>
-                  <div className="flex py-4 text-2xl">
+                  <div className="flex py-4 lg:text-xl text-sm">
                     <div className="pr-4">Age : </div>
                     <div>{sellpost[0].petAge}</div>
                   </div>
-                  <div className="flex py-4 text-2xl">
+                  <div className="flex py-4 lg:text-xl text-sm">
                     <div className="pr-4">Birthday : </div>
-                    <div>{BD.toLocaleDateString('th-TH', { year: 'numeric', month: 'long', day: 'numeric' })}</div>
+                    <div className=" hidden xl:flex">
+                      {BD.toLocaleDateString('th-TH', { year: 'numeric', month: 'long', day: 'numeric' })}
+                    </div>
+                    <div className="flex xl:hidden">
+                      {BD.toLocaleDateString('th-TH', { year: '2-digit', month: 'short', day: 'numeric' })}
+                    </div>
                   </div>
-                  <div className="flex py-4 text-2xl">
+                  <div className="flex py-4 lg:text-xl text-sm">
                     <div className="pr-4">Gender : </div>
                     <div>{sellpost[0].petGender}</div>
                   </div>
                 </div>
-                <div className="px-10">
-                  <div className="flex py-4 text-2xl">
+                <div className="xl:px-10 px-4">
+                  <div className="flex py-4 lg:text-xl text-sm">
                     <div className="pr-4">Location : </div>
                     <div>{sellpost[0].petLocation}</div>
                   </div>
-                  <div className="flex py-4 text-2xl">
+                  <div className="flex py-4 lg:text-xl text-sm">
                     <div className="pr-4">Price : </div>
                     <div>{sellpost[0].petPrice} $</div>
                   </div>
-                  <div className="flex py-4 text-2xl">
+                  <div className="flex py-4 lg:text-xl text-sm">
                     <div className="pr-4">type : </div>
                     <div>{sellpost[0].petType}</div>
                   </div>
-                  <div className="flex py-4 text-2xl">
+                  <div className="flex py-4 lg:text-xl text-sm">
                     <div className="pr-4">Gene : </div>
                     <div>{sellpost[0].petGene}</div>
                   </div>
                 </div>
               </div>
-              <div className="flex p-4 text-2xl">
+              <div className="flex py-4 lg:text-xl text-sm">
                 <div className="pr-4">Description : </div>
                 <div>{sellpost[0].petDescription}</div>
               </div>
