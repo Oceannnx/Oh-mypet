fcc810bc1fd9534d401dfc4f5003a395bc1469b2
หนมจีบ <onopingping@gmail.com>
2023-12-17 00:16:02 +0700

    fix : return error

--- sqlserver/controller/FetchSellPost/FetchSellPost.js
+++ sqlserver/controller/FetchSellPost/FetchSellPost.js
@@ -46,7 +46,7 @@ const fetchSellPost = async (req, res) => {
     })
     return res.status(200).send(MapSellPost)
   } catch (error) {
-    console.log(error)
+    res.status(500).send({ success: false })
   }
 }
 
