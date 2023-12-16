967deb6465b926123ea8a0cac9e4851e22da4ad2
หนมจีบ <onopingping@gmail.com>
2023-12-16 20:27:46 +0700

    fix : update test

--- sqlserver/controller/test/test.js
+++ sqlserver/controller/test/test.js
@@ -1,11 +1,8 @@
-const { dbConnect } = require('../dbConnect/database.js')
-
-const test = async (req, res) => {
+const test = (req, res) => {
   try {
-    const pool = await dbConnect()
-    const userID = req.body.userID // Example: /testRoute?userId=123
-    const result = await pool.query('SELECT count(_id) as count FROM user')
-    return res.status(200).send({ userID, count: result[0][0].count })
+    res.send({
+      message: 'Oh-MYPET',
+    })
   } catch (err) {
     console.log(err)
   }
