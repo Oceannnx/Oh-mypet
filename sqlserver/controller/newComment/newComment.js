c128463036a021d3fde6256a522397b2d6325c28
หนมจีบ <onopingping@gmail.com>
2023-12-17 17:15:16 +0700

    feat : new comment

--- sqlserver/controller/newComment/newComment.js
+++ sqlserver/controller/newComment/newComment.js
@@ -1,24 +1,36 @@
+const srs = require('secure-random-string')
+
 const { dbConnect } = require('../dbConnect/database.js')
 
 const newComment = async (req, res) => {
-  const { commentDesc, sellPostID } = req.body
-  const comment = {
-    userID: req.cookies.userID,
-    sellPostID,
-    commentDesc,
-    commentDate: new Date(Date.now()),
+  const { comment, advPostID } = req.body
+  if (comment === '') {
+    return res.status(400).send({
+      message: 'Enter Comment',
+      success: false,
+    })
   }
   try {
     const client = await dbConnect()
-    await client.query('INSERT INTO comment SET ?', comment)
+    const advComment = {
+      _id: await srs({ length: 24 }),
+      userID: req.cookies.userID,
+      advPostID,
+      commentText: comment,
+      commentDate: new Date(Date.now()),
+    }
+    await client.query('INSERT INTO comment SET ?', advComment)
     return res.status(201).send({
       message: 'New Comment Success',
       success: true,
     })
   } catch (error) {
-    console.log(error)
     return res.status(500).send({ success: false })
   }
 }
 
 module.exports = { newComment }
+// userID: new ObjectId(req.cookies.userID),
+// advPostID: new ObjectId(advPostID),
+// comment,
+// commentDate: new Date(Date.now()),
