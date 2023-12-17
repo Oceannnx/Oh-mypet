5ba9cb26b1834b3f1ed1c8f31f90767ed3082272
หนมจีบ <onopingping@gmail.com>
2023-12-17 18:36:10 +0700

    fix : delete post

--- sqlserver/controller/deletePost/deletePost.js
+++ sqlserver/controller/deletePost/deletePost.js
@@ -4,7 +4,7 @@ const deletePost = async (req, res) => {
   try {
     const client = await dbConnect()
     const postID = req.params.id
-    await client.query(`delete from sellpost where _id = "${postID}"`)
+    await client.query(`DELETE FROM sellPost WHERE sellPost._id = '${postID}'`)
     return res.status(200).send({ success: true })
   } catch (error) {
     return res.status(500).send({ message: 'Internal Server Error', success: false })
